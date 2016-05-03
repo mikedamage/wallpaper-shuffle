ccdae9b633815a279dccb150c236bface789e7d0
Mike Green <mike.is.green@gmail.com>
2016-05-03 12:25:47 -0400

    ES2015-ify the daemon script

--- es6/lib/daemon.js
+++ es6/lib/daemon.js
@@ -1,57 +1,63 @@
 #!/usr/bin/env node
 
-/* jshint node: true */
+import rotate   from './rotate-wallpaper';
+import notifier from 'node-notifier';
 
-'use strict';
+let instance;
 
-var timer, pattern, interval;
-var rotate   = require('./rotate-wallpaper');
-var notifier = require('node-notifier');
-var notify   = false;
+class Daemon {
+  constructor(params) {
+    this.pattern  = params.pattern;
+    this.interval = params.interval;
+    this.notify   = !!params.notify || false;
+  }
 
-process.on('message', function(params) {
-  pattern  = params.pattern;
-  interval = params.interval;
-  timer    = setInterval(rotate, interval, pattern);
-  notify   = params.notify;
+  startTimer() {
+    this.timer = setInterval(this.rotate.bind(this), this.interval);
 
-  rotate(pattern, params.notify).then(function() {
-    process.send({ running: true });
-  });
-});
-
-process.on('SIGUSR1', function() {
-  if (timer) {
-    clearInterval(timer);
-    timer = 0;
-
-    if (notify) {
-      notifier.notify({
-        title: 'Wallpaper Shuffle',
-        message: 'Paused!'
-      });
-    }
-  } else {
-    if (notify) {
-      notifier.notify({
-        title: 'Wallpaper Shuffle',
-        message: 'Unpaused!'
-      });
-    }
-
-    rotate(pattern, notify).then(function() {
-      timer = setInterval(rotate, interval, pattern, notify);
+    this.notify && notifier.notify({
+      title: 'Wallpaper Shuffle',
+      message: 'Started!'
     });
   }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    this.timer = 0;
+
+    this.notify && notifier.notify({
+      title: 'Wallpaper Shuffle',
+      message: 'Stopped!'
+    });
+  }
+
+  toggleTimer() {
+    return (this.timer ? this.stopTimer() : this.startTimer());
+  }
+
+  rotate() {
+    rotate(this.pattern, this.notify);
+  }
+}
+
+process.on('message', (params) => {
+  instance = new Daemon(params);
+  instance.startTimer();
+  instance.rotate().then(() => process.send({ running: true }));
 });
 
-process.on('SIGUSR2', function() {
-  if (pattern) {
-    rotate(pattern, notify);
+process.on('SIGUSR1', () => {
+  if (!instance) return;
+  instance.toggleTimer();
+});
+
+process.on('SIGUSR2', () => {
+  if (instance.pattern) {
+    instance.rotate();
   }
 });
 
-process.on('SIGTERM', function() {
-  clearInterval(timer);
+process.on('SIGTERM', () => {
+  instance.stopTimer();
   process.exit();
 });
